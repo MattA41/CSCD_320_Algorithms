3b5d6d1f70f4262f8668f07e02567417039a5ec6
madams41 <madams41@ewu.edu>
2024-03-09 14:24:27 -0800

    Used Dominic MacIsaac python file for more testing and finished prog

--- project4_cscd320_algorithms/prog4/src/TopoSort.java
+++ project4_cscd320_algorithms/prog4/src/TopoSort.java
@@ -5,12 +5,18 @@ public class TopoSort {
     {
         //System.out.println("Hello world!");
         int[][]dag = readFile(args[0]);
-        //TODO find magic 0
-        for(int i =0; i<dag.length; i++){
-            for(int j =0; j < dag[0].length; j++){
-                System.out.println(i + " " +  dag[i][j]);
+        int size = 0;
+        String print= "";
+        for(int i =0; i< dag.length; i++) {
+            String out = saveOrder(dag, i);
+            //System.out.println(out);
+            if(out.length() > size){
+                size = out.length();
+                print = out;
             }
         }
+        System.out.println(print);
+
     }
     public static int[][] readFile(String filename){
         File dagFile = new File(filename);
@@ -28,19 +34,31 @@ public class TopoSort {
             }
             // create array of size count, count = num of nodes
             int[][] dag = new int[count][count];
+            //fill with -1 for knowing when to stop
+            for(int i =0; i<dag.length; i++){
+                for(int j =0; j < dag[0].length; j++){
+                    dag[i][j] = -1;
+                }
+            }
             //get data from dag file
             try
             {
                 Scanner readDag = new Scanner(dagFile);
                 while (readDag.hasNext()){
                     String rawIn = readDag.nextLine();
+                    //split into node and node children
                     String[] parentNode = rawIn.split(":");
                     int dagx = Integer.parseInt(parentNode[0]);
                     if(parentNode.length >= 2){
-                        String[] childNodes = parentNode[1].split(",");
-                        //System.out.println(parentNode[0] + " " + childNodes[0]);
-                        for(int i = 0; i< childNodes.length; i++) {
-                            dag[dagx][i] = Integer.parseInt(childNodes[i]);
+                        // split the list of nodes that are under parent
+                        String[] childNodesString = parentNode[1].split(",");
+                        int[] childNodes = new int[childNodesString.length];
+                        for(int j =0; j < childNodesString.length;j++){
+                            childNodes[j] = Integer.parseInt(childNodesString[j]);
+                        }
+                        Arrays.sort(childNodes);
+                        for(int i = 0; i< childNodesString.length; i++) {
+                            dag[dagx][i] = childNodes[i];
                         }
                     }else {
                         //if it does not have children it gets -1
@@ -55,5 +73,19 @@ public class TopoSort {
             }
 
         return dag;
+    }
+    //inital call is dag 0
+    public static String saveOrder(int[][] dag, int index){
+
+        String ret = Integer.toString(index);
+        //check if path has been gone down before
+        Boolean cont = true;
+        for(int i = 0; i < dag[index].length && cont; i++){
+            if (dag[index][i] != -1 ){
+                ret = ret + ","+  saveOrder(dag,dag[index][i]);
+                cont = false;
+            }
+        }
+        return ret;
     }
 }
